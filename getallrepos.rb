514c798431220d65e910cc891431084e79c4efe2
Hamza Anis <hamzaanis9514@gmail.com>
2020-08-05 04:06:23 +0500

    checking for valid arguments

--- getallrepos.rb
+++ getallrepos.rb
@@ -1,11 +1,13 @@
+#!/usr/bin/ruby
 require 'net/http'
 require 'json'
 
-unless ARGV.length == 1
-    puts "Enter github username in the command line."
+unless ARGV.length >= 1
+    puts "\tusage: getallrepos.rn [username] [directory]"
+    raise ArgumentError, "enter github username in the command line."
+else
+    url = 'https://api.github.com/users/'+ARGV[0]+'/repos'
+    uri = URI(url)
+    response = Net::HTTP.get(uri)
+    JSON.load(response).each { |repo| %x[git clone #{repo["git_url"]} ]}
 end
-url = 'https://api.github.com/users/'+ARGV[0]+'/repos'
-uri = URI(url)
-response = Net::HTTP.get(uri)
-# puts JSON.parse(response)
-JSON.load(response).each { |repo| %x[git clone #{repo["git_url"]} ]}
