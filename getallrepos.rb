2f2ad593282727020c2796ee2054effb267a77c4
Hamza Anis <hamzaanis9514@gmail.com>
2020-08-05 04:25:29 +0500

    ⚡Cloning to path passed by argument

--- getallrepos.rb
+++ getallrepos.rb
@@ -9,6 +9,13 @@ else
     url = 'https://api.github.com/users/'+ARGV[0]+'/repos'
     uri = URI(url)
     response = Net::HTTP.get(uri)
-    Dir.mkdir(ARGV[0]) unless File.exists?(ARGV[0])
-    JSON.load(response).each { |repo| %x[git -C #{ARGV[0]} clone  #{repo["git_url"]} ]}
+    if ARGV.length == 2
+        if Dir.exist?(ARGV[1])==false
+            Dir.mkdir(ARGV[1]) unless File.exists?(ARGV[1])
+        end
+        JSON.load(response).each { |repo| %x[git -C #{ARGV[1]} clone  #{repo["git_url"]} ]}
+    else
+        Dir.mkdir(ARGV[0]) unless File.exists?(ARGV[0])
+        JSON.load(response).each { |repo| %x[git -C #{ARGV[0]} clone  #{repo["git_url"]} ]}
+    end
 end
